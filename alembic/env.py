3782c9e6975023c8465433426ee36b3251f01763
Saeeda <saeedashukorpyp@gmail.com>
2025-05-23 14:22:39 +0100

    chore: Dockerfile and docker-compose

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,4 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -35,7 +36,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = config.get_main_option("sqlalchemy.url") or os.getenv("DATABASE_URL")
     context.configure(
         url=url,
         target_metadata=target_metadata,
